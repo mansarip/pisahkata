2d624f23238e0796faeda2ac0da8aa6ca32e20b7
Luqman <luqmanrasa@gmail.com>
2023-08-21 09:23:32 +0800

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
+      <Analytics />
     </ChakraProvider>
   </React.StrictMode>
 );
