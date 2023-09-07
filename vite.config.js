093cd49b6f4be632245ec067b4feba733a0e82e5
Luqman <luqmanrasa@gmail.com>
2023-09-07 10:09:03 +0800

    added pwa + offline

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["**/*"], // static assets in the public folder
+      manifest: {
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "Huruf Arab",
+        short_name: "Huruf Arab",
+        description:
+          "Alat bantuan visual untuk memahami cara penyambungan huruf Arab di. Direka untuk guru-guru dan pelajar yang keliru bagaimana rupa huruf jika bersambung.",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
